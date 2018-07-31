5c9fb81323b9ee68f50e22858d50fefb1e3c85d8
nikeyol <zj-1314@163.com>
2018-07-31 22:14:35 +0800

    first commit for tracker demo

---
+++ src/main/java/com/pivotal/demo/tracker/DemoTrackerApp.java
@@ -0,0 +1,13 @@
+package com.pivotal.demo.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrackerApp {
+    public static void main(String[] args){
+        SpringApplication.run(DemoTrackerApp.class,args);
+    }
+}
+
+
